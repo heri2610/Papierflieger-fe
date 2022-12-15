39cc505a665b5f6b6283083140faebfdc555c728
heri2610 <suhaeriheri45@gmail.com>
2022-12-15 15:24:47 +0700

    conflik

--- src/components/Navbar/NavigationBar.jsx
+++ src/components/Navbar/NavigationBar.jsx
@@ -1,13 +1,6 @@
 import React, { useState } from "react";
 // import { Link } from "react-router-dom";
-import {
-  Navbar,
-  Nav,
-  Container,
-  Offcanvas,
-  Button,
-  Modal,
-} from "react-bootstrap";
+import { Navbar, Nav, Container, Offcanvas, Button, Modal } from "react-bootstrap";
 import Login from "../Login/Login";
 import brand from "../../../assets/images/logo.svg";
 import "./navbar.scss";
@@ -19,30 +12,15 @@ const NavigationBar = () => {
   const handleShow = () => setShow(true);
   return (
     <div className="container-fluid mb-3 fixed-top">
-      <Navbar
-        expand="lg"
-        className="justify-content-center bg-white rounded shadow-sm"
-      >
+      <Navbar expand="lg" className="justify-content-center bg-white rounded shadow-sm">
         <Container fluid className="navbar-content">
           {/* <Link to={"/"}> */}
-          <img
-            src={brand}
-            alt="Logo"
-            width="70"
-            className="d-inline-block align-text-top"
-          />
+          <img src={brand} alt="Logo" width="70" className="d-inline-block align-text-top" />
           {/* </Link> */}
           <Navbar.Toggle aria-controls="offcanvasNavbar-expand-lg" />
-          <Navbar.Offcanvas
-            id="offcanvasNavbar-expand-lg"
-            aria-labelledby="offcanvasNavbarLabel-expand-lg"
-            placement="end"
-          >
+          <Navbar.Offcanvas id="offcanvasNavbar-expand-lg" aria-labelledby="offcanvasNavbarLabel-expand-lg" placement="end">
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title
-                className="fw-bold"
-                id="offcanvasNavbarLabel-expand-lg"
-              >
+              <Offcanvas.Title className="fw-bold" id="offcanvasNavbarLabel-expand-lg">
                 Menu
               </Offcanvas.Title>
             </Offcanvas.Header>
@@ -75,11 +53,7 @@ const NavigationBar = () => {
         <Modal.Body className="softblue">
           <Modal.Title className="mx-6">Masuk</Modal.Title>
           <Login />
-          <Button
-            className="mt-3 w-75 mx-7"
-            variant="primary"
-            onClick={handleClose}
-          >
+          <Button className="mt-3 w-75 mx-7" variant="primary" onClick={handleClose}>
             Masuk
           </Button>
           <br className="mt-2" /> Belum punya akun? Silakan{" "}
