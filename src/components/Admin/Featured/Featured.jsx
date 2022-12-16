624ef0d88bfbb5017b67e4401f83ec63376a34d5
Sri Endah <sriendah@upi.edu>
2022-12-16 18:34:22 +0700

    fix featured

--- src/components/Admin/Featured/Featured.jsx
+++ src/components/Admin/Featured/Featured.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css'
+import 'react-circular-progressbar/dist/styles.css';
 import './Featured.scss';
 
 const Featured = () => {
@@ -8,10 +8,22 @@ const Featured = () => {
     <div className='featured'>
       <div className="top">
         <h6 className="title-feature">Total Pendapatan</h6>
+        <FiMoreVertical/>
       </div>
       <div className="bottom">
         <div className="featured-chart">
           <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+        </div>
+        <p className="sales-title">Total penjualan hari ini</p>
+        <p className="sales-amount">Rp. 34.000.000</p>
+        <p className="desc">Pembayaran terakhir masih berlangsung</p>
+        <div className="summary">
+          <div className="item">
+            <div className="item-title">Target</div>
+              <div className="item-result">
+                <div className="result-amount">Rp. 1.500.000.000</div>
+              </div>
+          </div>
         </div>
       </div>
     </div>
