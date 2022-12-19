a2cd82ce218ce744effabaa10b62a6909cbeb2a5
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2022-12-19 13:42:06 +0700

    menghubungkan API bandara ke pencarian

--- src/components/User/SearchTicket/SearchTicket.jsx
+++ src/components/User/SearchTicket/SearchTicket.jsx
@@ -1,12 +1,17 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Form } from "react-bootstrap";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { getAirport } from "../../../store/actions/airport";
 
 import "./searchTicket.scss";
 
 export default function SearchTicket() {
-  // const { data } = useSelector((state) => state.airportReducer);
+  const { data } = useSelector((state) => state.airportReducer);
+  const [type, setType] = useState("");
+
+  const handleType = (event) => {
+    setType(event.target.value);
+  };
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -27,61 +32,77 @@ export default function SearchTicket() {
               <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4">
                 <div className="col">
                   <div className="form-floating">
-                    <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
-                      <option defaultValue>Jakarta (CGK)</option>
-                      <option value="1">Sydney</option>
-                      <option value="2">Tokyo</option>
-                      <option value="3">Jeddah</option>
+                    <select className="form-select" id="floatingSelectFrom" required>
+                      {data &&
+                        data?.map((airport) => (
+                          <option key={airport.id} value={airport.id}>
+                            <div>
+                              <strong>{airport.city}</strong>
+                              <small> | {airport.airportName} ({airport.cityCode})</small>
+                            </div>
+                          </option>
+                        ))
+                      }
                     </select>
-                    <label htmlFor="floatingSelect">Dari</label>
+                    <label htmlFor="floatingSelectFrom">Dari</label>
                   </div>
                 </div>
                 <div className="col">
                   <div className="form-floating">
-                    <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
-                      <option defaultValue>Singapura (SIN)</option>
-                      <option value="1">Sydney</option>
-                      <option value="2">Tokyo</option>
-                      <option value="3">Jeddah</option>
+                    <select className="form-select" id="floatingSelectTo" required>
+                      {data &&
+                        data?.map((airport) => (
+                          <option key={airport.id} value={airport.id}>
+                            <div>
+                              <strong>{airport.city}</strong>
+                              <small> | {airport.airportName} ({airport.cityCode})</small>
+                            </div>
+                          </option>
+                        ))
+                      }
                     </select>
-                    <label htmlFor="floatingSelect">Ke</label>
+                    <label htmlFor="floatingSelectTo">Ke</label>
                   </div>
                 </div>
                 <div className="col">
                   <div className="form-floating">
-                    <input type="number" min="1" className="form-control" id="inputPenumpang" placeholder="name@example.com" />
+                    <input type="number" min="1" max="10" className="form-control" id="inputPenumpang" />
                     <label htmlFor="inputPenumpang">Penumpang</label>
                   </div>
                 </div>
                 <div className="col">
                   <div className="form-floating">
-                    <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
-                      <option defaultValue="one-way">One-Way</option>
-                      <option value="round-trip">Round-trip</option>
-                    </select>
-                    <label htmlFor="floatingSelect">Keberangkatan</label>
-                  </div>
-                </div>
-              </div>
-              <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 mb-3">
-                <div className="col">
-                  <div className="form-floating">
-                    <select className="form-select" id="floatingSelect" aria-label="Floating label select example">
+                    <select className="form-select" id="floatingSelect">
                       <option defaultValue="ekonomi">Ekonomi</option>
                       <option value="bisnis">Bisnis</option>
                     </select>
                     <label htmlFor="floatingSelect">Kelas</label>
                   </div>
                 </div>
+              </div>
+              <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 mb-3">
                 <div className="col">
                   <div className="form-floating">
-                    <input type="date" className="form-control" id="inputPergi" placeholder="name@example.com" />
+                    <select className="form-select" id="floatingSelect" onChange={handleType} value={type}>
+                      <option defaultValue="one-way">Sekali Jalan</option>
+                      <option value="round-trip">Pergi Pulang</option>
+                    </select>
+                    <label htmlFor="floatingSelect">Keberangkatan</label>
+                  </div>
+                </div>
+                <div className="col">
+                  <div className="form-floating">
+                    <input type="date" className="form-control" id="inputPergi" required />
                     <label htmlFor="inputPergi">Pergi</label>
                   </div>
                 </div>
                 <div className="col">
                   <div className="form-floating">
-                    <input type="date" className="form-control" id="inputPulang" placeholder="name@example.com" />
+                    {
+                      type === 'round-trip' ?
+                        <input type="date" className="form-control" id="inputPulang" required />
+                        : <input type="date" className="form-control" id="inputPulang" disabled />
+                    }
                     <label htmlFor="inputPulang">Pulang</label>
                   </div>
                 </div>
