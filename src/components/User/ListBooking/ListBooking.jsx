c694ac9fd443fc3d2ba732a925e02a327d8e12f9
Nurul Fadhila <nurulfadhilamuel@gmail.com>
2022-12-22 15:09:17 +0700

    mengubah list booking

--- src/components/User/ListBooking/ListBooking.jsx
+++ src/components/User/ListBooking/ListBooking.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Container, Row, Col, Accordion } from "react-bootstrap";
 import Arrow from "../../../assets/images/Vector.svg";
 import logocard from "../../../assets/images/Logo-card.svg";
 import "./ListBooking.scss";
@@ -7,32 +8,225 @@ const ListBooking = () => {
   return (
     <div className="img-banner">
       <div className="container">
-        <h4 className="my-3 fw-bold">Daftar Pemesanan</h4>
-        <div className="container card mb-5">
-          <div className="card-body d-flex align-items-start align-items-md-center justify-content-between">
-            <div className="d-flex align-items-center">
-              <div className="col-md-4 me-4 p-lg-3">
-                <img
-                  className="img-fluid"
-                  variant="top"
-                  src={logocard}
-                  alt=""
-                />
+        <h4 className="my-2 mx-3 mb-3 fw-bold">Daftar Pemesanan</h4>
+        <Container fluid className="pb-4 penerbangan mb-2">
+          <Row>
+            <Col md="12">
+              <div className="bg-white p-3 rounded">
+                <Row>
+                  <Col>
+                    <Accordion>
+                      <Accordion.Item eventKey="0" className="border border-0">
+                        <Accordion.Header>
+                          <Col md={3} lg={2}>
+                            <img
+                              src={logocard}
+                              className="img-fluid logo-card"
+                              alt="logo-card"
+                            />
+                          </Col>
+                          <Col className=" mt-3 mt-md-0">
+                            <h5>Penerbangan</h5>
+                            <div className="hstack">
+                              <h3>
+                                <strong>Banda Aceh</strong>
+                              </h3>
+                              <img src={Arrow} alt="arrow" className="mx-3" />
+                              <h3>
+                                <strong>Jakarta</strong>
+                              </h3>
+                            </div>
+                            <h5>Selasa, 10 Januari 2022</h5>
+                          </Col>
+                          <h5 className="d-inline ms-auto me-3 mb-0">
+                            Pratinjau Rincian
+                          </h5>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                          <div id="timeline-content" className="border-bottom">
+                            <ul className="timeline">
+                              <li
+                                className="event"
+                                data-date="08.00"
+                                data-day="16 Desember"
+                              >
+                                <h2>Jakarta</h2>
+                                <p>
+                                  Soekarno Hatta International Airport (CGK)
+                                </p>
+                                <p>
+                                  Armada : PapierFlieger, Boeing 737
+                                  <br />
+                                  Durasi Tebang : 3 jam 40 menit
+                                </p>
+                              </li>
+                              <li
+                                className="event"
+                                data-date="08.00"
+                                data-day="16 Desember"
+                              >
+                                <h2>Jakarta</h2>
+                                <p>
+                                  Soekarno Hatta International Airport (CGK)
+                                </p>
+                                <p>
+                                  Armada : PapierFlieger, Boeing 737
+                                  <br />
+                                  Durasi Tebang : 3 jam 40 menit
+                                </p>
+                              </li>
+                              <li
+                                className="event"
+                                data-date="08.00"
+                                data-day="16 Desember"
+                              >
+                                <h2>Jakarta</h2>
+                                <p>
+                                  Soekarno Hatta International Airport (CGK)
+                                </p>
+                                <p>
+                                  Armada : PapierFlieger, Boeing 737
+                                  <br />
+                                  Durasi Tebang : 3 jam 40 menit
+                                </p>
+                              </li>
+                            </ul>
+                          </div>
+                          <Row>
+                            <Col>
+                              <h5>Komplementer Bagasi</h5>
+                              <p>
+                                Harga tiket sudah termasuk beban bagasi
+                                per-orang dengan rincian beban sebagasi berikut
+                                :
+                              </p>
+                              <h5 style={{ paddingTop: 0 }}>
+                                <i className="bi bi-file-zip-fill"></i>{" "}
+                                Tas/Koper 7 kg x 1
+                              </h5>
+                              <p className="text-primary">
+                                <strong>Tas/Koper yang diperiksa</strong>
+                              </p>
+                            </Col>
+                          </Row>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+                  </Col>
+                </Row>
               </div>
-              <div className="col-5 mt-md-0">
-                <p>Penerbangan</p>
-                <h3 className="card-title fw-bold fs-5">Banda Aceh</h3>
-                <p className="card-text text-muted">Selasa 10 Januari 2022</p>
+            </Col>
+          </Row>
+        </Container>
+        <Container fluid className="pb-4 penerbangan mb-2">
+          <Row>
+            <Col md="12">
+              <div className="bg-white p-3 rounded">
+                <Row>
+                  <Col>
+                    <Accordion>
+                      <Accordion.Item eventKey="0" className="border border-0">
+                        <Accordion.Header>
+                          <Col md={3} lg={2}>
+                            <img
+                              src={logocard}
+                              className="img-fluid logo-card"
+                              alt="logo-card"
+                            />
+                          </Col>
+                          <Col className=" mt-3 mt-md-0">
+                            <h5>Penerbangan</h5>
+                            <div className="hstack">
+                              <h3>
+                                <strong>Banda Aceh</strong>
+                              </h3>
+                              <img src={Arrow} alt="arrow" className="mx-3" />
+                              <h3>
+                                <strong>Jakarta</strong>
+                              </h3>
+                            </div>
+                            <h5>Selasa, 10 Januari 2022</h5>
+                          </Col>
+                          <h5 className="d-inline ms-auto me-3 mb-0">
+                            Pratinjau Rincian
+                          </h5>
+                        </Accordion.Header>
+                        <Accordion.Body>
+                          <div id="timeline-content" className="border-bottom">
+                            <ul className="timeline">
+                              <li
+                                className="event"
+                                data-date="08.00"
+                                data-day="16 Desember"
+                              >
+                                <h2>Jakarta</h2>
+                                <p>
+                                  Soekarno Hatta International Airport (CGK)
+                                </p>
+                                <p>
+                                  Armada : PapierFlieger, Boeing 737
+                                  <br />
+                                  Durasi Tebang : 3 jam 40 menit
+                                </p>
+                              </li>
+                              <li
+                                className="event"
+                                data-date="08.00"
+                                data-day="16 Desember"
+                              >
+                                <h2>Jakarta</h2>
+                                <p>
+                                  Soekarno Hatta International Airport (CGK)
+                                </p>
+                                <p>
+                                  Armada : PapierFlieger, Boeing 737
+                                  <br />
+                                  Durasi Tebang : 3 jam 40 menit
+                                </p>
+                              </li>
+                              <li
+                                className="event"
+                                data-date="08.00"
+                                data-day="16 Desember"
+                              >
+                                <h2>Jakarta</h2>
+                                <p>
+                                  Soekarno Hatta International Airport (CGK)
+                                </p>
+                                <p>
+                                  Armada : PapierFlieger, Boeing 737
+                                  <br />
+                                  Durasi Tebang : 3 jam 40 menit
+                                </p>
+                              </li>
+                            </ul>
+                          </div>
+                          <Row>
+                            <Col>
+                              <h5>Komplementer Bagasi</h5>
+                              <p>
+                                Harga tiket sudah termasuk beban bagasi
+                                per-orang dengan rincian beban sebagasi berikut
+                                :
+                              </p>
+                              <h5 style={{ paddingTop: 0 }}>
+                                <i className="bi bi-file-zip-fill"></i>{" "}
+                                Tas/Koper 7 kg x 1
+                              </h5>
+                              <p className="text-primary">
+                                <strong>Tas/Koper yang diperiksa</strong>
+                              </p>
+                            </Col>
+                          </Row>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+                  </Col>
+                </Row>
               </div>
-              <div className="col-md mt-md-0 vector-right">
-                <img src={Arrow} alt="arrow" className="mx-3 mt-2" />
-              </div>
-              <div className="col-md mt-3 mt-md-0">
-                <h3 className="card-title fw-bold fs-5 mt-3">Jakarta</h3>
-              </div>
-            </div>
-          </div>
-        </div>
+            </Col>
+          </Row>
+        </Container>
       </div>
     </div>
   );
