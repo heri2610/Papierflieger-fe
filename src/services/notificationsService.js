4570e36e044742e0b096c5cbee618b9e53cae972
heri2610 <suhaeriheri45@gmail.com>
2022-12-31 15:43:19 +0700

    menambahkan count notifikasi

--- src/services/notificationsService.js
+++ src/services/notificationsService.js
@@ -9,6 +9,14 @@ const notifService = {
       throw err;
     }
   },
+  CountNotif: async function () {
+    try {
+      const response = await API.get("/count-notif");
+      return response;
+    } catch (err) {
+      throw err;
+    }
+  },
   seeNotif: async function (id) {
     try {
       const response = await API.put(`/notifications/${id}`);
