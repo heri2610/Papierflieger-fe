a84b0e5dd85c1d817c6b97ad1593d59307e9afce
Bregsi Atingsari Julastri <bregsiaju@gmail.com>
2023-01-01 21:03:30 +0700

    merevisi tampilan responsif navbar

--- src/components/Navbar/DropdownNav.jsx
+++ src/components/Navbar/DropdownNav.jsx
@@ -1,28 +1,56 @@
-import { DropdownButton, Dropdown } from "react-bootstrap";
+import { DropdownButton, Dropdown, Nav } from "react-bootstrap";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/actions/auth.js";
 import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import "./navbar.scss";
 
 function DropdownNav() {
-  // const a = localStorage.getItem("accessToken") === "A-*dmin?&&%mlm-plgsnwngbuay-$563iedjnjdxgdj" ? true : false;
   const { isAdmin } = useSelector((state) => state.authReducer);
   const dispatch = useDispatch();
-  // console.log(isAdmin);
+
+  const [matches, setMatches] = useState(
+    window.matchMedia("(min-width: 768px)").matches
+  );
+
+  useEffect(() => {
+    window
+      .matchMedia("(min-width: 768px)")
+      .addEventListener("change", (e) => setMatches(e.matches));
+  }, []);
+
   return (
-    <DropdownButton title={window.localStorage.getItem("user")} align="end" id="dropdown-menu-align-end">
-      <Dropdown.Item href="/user/profile">Profil</Dropdown.Item>
-      {isAdmin && <Dropdown.Item href="/admin">Dashboard Admin</Dropdown.Item>}
-      <Dropdown.Item href="/user/wishlist">Wishlist</Dropdown.Item>
-      <Dropdown.Item href="/user/transaction/history">Riwayat</Dropdown.Item>
-      <Dropdown.Item
-        onClick={() => {
-          dispatch(logout());
-          window.location.reload();
-        }}
-      >
-        Logout
-      </Dropdown.Item>
-    </DropdownButton>
+    <>
+      {matches && (
+        <DropdownButton title={window.localStorage.getItem("user")} align="end" id="dropdown-menu-align-end">
+          <Dropdown.Item href="/user/profile">Profil</Dropdown.Item>
+          {isAdmin && <Dropdown.Item href="/admin">Dashboard Admin</Dropdown.Item>}
+          <Dropdown.Item href="/user/wishlist">Wishlist</Dropdown.Item>
+          <Dropdown.Item href="/user/transaction/history">Riwayat</Dropdown.Item>
+          <Dropdown.Item
+            onClick={() => {
+              dispatch(logout());
+              window.location.reload();
+            }}
+          >
+            Logout
+          </Dropdown.Item>
+        </DropdownButton>
+      )}
+      {!matches && (
+        <Nav>
+          <Nav.Link className="me-4" href="/user/profile">Profil</Nav.Link>
+          {isAdmin && <Nav.Link className="me-4" href="/admin">Dashboard Admin</Nav.Link>}
+          <Nav.Link className="me-4" href="/user/wishlist">Wishlist</Nav.Link>
+          <Nav.Link className="me-4" href="/user/transaction/history">Riwayat Pemesanan</Nav.Link>
+          <Nav.Link className="me-4 text-danger" onClick={() => {
+            dispatch(logout());
+            window.location.reload();
+          }}>Logout
+          </Nav.Link>
+        </Nav>
+      )}
+    </>
   );
 }
 
